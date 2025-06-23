e5061a3e1c8c74b14959b3f3c32f0abd919f14dd
Molnia00 <bilankati@gmail.com>
2025-06-23 14:03:42 +0300

    logout corrected

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,6 +3,7 @@ import { User } from "../models/User.js";
 import bcrypt from 'bcrypt';
 import { Session } from "../models/Session.js";
 import crypto from 'node:crypto';
+import { Types } from "mongoose";
 
 export async function registerUser(payload) {
     const user = await User.findOne({ email: payload.email });
@@ -46,8 +47,12 @@ export async function loginUser(email, password) {
 
 
 
-export async function logoutUser(sessionId, refreshToken) {
-   await Session.deleteOne({_id : sessionId, refreshToken})
+export async function logoutUser(sessionId) {
+    if (!Types.ObjectId.isValid(sessionId)) {
+        console.error("Invalid sessionId provided for logout:", sessionId);
+        throw new createHttpError.Unauthorized('Invalid session ID format');
+    }
+   await Session.deleteOne({_id : sessionId})
 }
 
 export async function refreshUser(sessionId, refreshToken) {
